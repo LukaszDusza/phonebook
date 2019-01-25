655bb766b39ca6ef35c02aaf70b9c19bff86730a
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-01-25 12:51:00 +0100

    initial 6, last for online tutorial Spring Boot examples

--- src/main/java/devlab/phonebook/PhonebookApplication.java
+++ src/main/java/devlab/phonebook/PhonebookApplication.java
@@ -16,17 +16,16 @@ public class PhonebookApplication {
 
     }
 
-    /* zaczniemy od tworzenia klasy modelowej w naszym projekcie. Aplikacja ma za zadanie gromadzić nasze kontakty do innych osób.
+    /*
+
+     Aplikacja ma za zadanie gromadzić nasze kontakty do innych osób.
      dla uproszeczenia tutaj będziemy przechowywać imię, nazwisko, numer telefonu, adres jako miejscowość, kategorię kotaktu,
      ranking, czyli coś po czym w przyszłości możemy wyswietlać kontakty w kolejności np. ze względu na częstość używania
      i tagi, po których łatwo wyszukamy konkretną grupę osób.
 
-     Pola te tworzymy głównie dla przykładu relacji pomiędzy encjami w bazie danych.
-     Będziemy programować relacje:
-     ManyToMany, OneToMany, ManyToOne, OneToOne.
+     Relacje:  ManyToMany, OneToMany, ManyToOne.
 
-     Pokażę również jak skonfigurować bibliotekę Lombok, która załatwia nam całość standardowego kodu, jaki musimy napisać,
-     aby klasa była używalna, czyli getery setery, konstruktory itp.*/
+*/
 
 }
 
